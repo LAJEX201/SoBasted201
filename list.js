ec58df8389aac9618cc7d6ccdc97039574cb43a5
Xochil73 <xochilsquaglia@yahoo.com>
2018-10-30 21:26:45 -0700

    xochil made the 'start over' button work

--- list.js
+++ list.js
@@ -31,151 +31,161 @@ var allRecipesSelected15;
 // var allRecipesSelected18;
 
 var staticIngredientsArrayCreator = function(ingredient){
-    if (!uniqueIngre.includes(ingredient.name)){
-      uniqueIngre.push(ingredient.name)
-      ingreQty[ingredient.name] = ingredient.quantity
-      ingreMeas[ingredient.name] = ingredient.measurementunit
-      ingreLoc[ingredient.name] = ingredient.location
-    } else {
-      ingreQty[ingredient.name] += ingredient.quantity
-    }
+  if (!uniqueIngre.includes(ingredient.name)){
+    uniqueIngre.push(ingredient.name);
+    ingreQty[ingredient.name] = ingredient.quantity;
+    ingreMeas[ingredient.name] = ingredient.measurementunit;
+    ingreLoc[ingredient.name] = ingredient.location;
+  } else {
+    ingreQty[ingredient.name] += ingredient.quantity;
+  }
 };
 
 if(localStorage.getItem('appetizer-1')){
   allRecipesSelected1 = JSON.parse(localStorage.getItem('appetizer-1'));
   for(var c = 0; c < allRecipesSelected1.ingredients.length; c++){
     staticIngredientsArrayCreator(allRecipesSelected1.ingredients[c]);
-    
+
   }
-};
+}
 if(localStorage.getItem('appetizer-2')){
   allRecipesSelected2 = JSON.parse(localStorage.getItem('appetizer-2'));
-    for(var c = 0; c < allRecipesSelected2.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected2.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('appetizer-3')){
-    allRecipesSelected3 = JSON.parse(localStorage.getItem('appetizer-3'));
-    for(var c = 0; c < allRecipesSelected3.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected3.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('main-dish-1')){
-    allRecipesSelected4 = JSON.parse(localStorage.getItem('main-dish-1'));
-    for(var c = 0; c < allRecipesSelected4.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected4.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('main-dish-2')){
-    allRecipesSelected5 = JSON.parse(localStorage.getItem('main-dish-2'));
-    for(var c = 0; c < allRecipesSelected5.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected5.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('main-dish-3')){
-    allRecipesSelected6 = JSON.parse(localStorage.getItem('main-dish-3'));
-    for(var c = 0; c < allRecipesSelected6.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected6.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('side-dish-1')){
-    allRecipesSelected7 = JSON.parse(localStorage.getItem('side-dish-1'));
-    for(var c = 0; c < allRecipesSelected7.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected7.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('side-dish-2')){
-    allRecipesSelected8 = JSON.parse(localStorage.getItem('side-dish-2'));
-    for(var c = 0; c < allRecipesSelected8.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected8.ingredients[c]);
-      
-    }
-  };
+  for(var c = 0; c < allRecipesSelected2.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected2.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('appetizer-3')){
+  allRecipesSelected3 = JSON.parse(localStorage.getItem('appetizer-3'));
+  for(var c = 0; c < allRecipesSelected3.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected3.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('main-dish-1')){
+  allRecipesSelected4 = JSON.parse(localStorage.getItem('main-dish-1'));
+  for(var c = 0; c < allRecipesSelected4.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected4.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('main-dish-2')){
+  allRecipesSelected5 = JSON.parse(localStorage.getItem('main-dish-2'));
+  for(var c = 0; c < allRecipesSelected5.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected5.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('main-dish-3')){
+  allRecipesSelected6 = JSON.parse(localStorage.getItem('main-dish-3'));
+  for(var c = 0; c < allRecipesSelected6.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected6.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('side-dish-1')){
+  allRecipesSelected7 = JSON.parse(localStorage.getItem('side-dish-1'));
+  for(var c = 0; c < allRecipesSelected7.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected7.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('side-dish-2')){
+  allRecipesSelected8 = JSON.parse(localStorage.getItem('side-dish-2'));
+  for(var c = 0; c < allRecipesSelected8.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected8.ingredients[c]);
+
+  }
+}
 if(localStorage.getItem('side-dish-3')){
   allRecipesSelected9 = JSON.parse(localStorage.getItem('side-dish-3'));
   for(var c = 0; c < allRecipesSelected9.ingredients.length; c++){
     staticIngredientsArrayCreator(allRecipesSelected9.ingredients[c]);
-    
+
   }
-};
+}
 if(localStorage.getItem('dessert-1')){
   allRecipesSelected10 = JSON.parse(localStorage.getItem('dessert-1'));
   for(var c = 0; c < allRecipesSelected10.ingredients.length; c++){
     staticIngredientsArrayCreator(allRecipesSelected10.ingredients[c]);
-    
+
   }
-};
+}
 if(localStorage.getItem('dessert-2')){
   allRecipesSelected11 = JSON.parse(localStorage.getItem('dessert-2'));
   for(var c = 0; c < allRecipesSelected11.ingredients.length; c++){
     staticIngredientsArrayCreator(allRecipesSelected11.ingredients[c]);
-    
-    }
-  };
-  if(localStorage.getItem('dessert-3')){
-    allRecipesSelected12 = JSON.parse(localStorage.getItem('dessert-3'));
-    for(var c = 0; c < allRecipesSelected12.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected12.ingredients[c]);
-      
-    }
-  };
-  if(localStorage.getItem('beverages-1')){
-    allRecipesSelected13 = JSON.parse(localStorage.getItem('beverages-1'));
-    for(var c = 0; c < allRecipesSelected13.ingredients.length; c++){
-      staticIngredientsArrayCreator(allRecipesSelected13.ingredients[c]);
-    
-    }
-  };
+
+  }
+}
+if(localStorage.getItem('dessert-3')){
+  allRecipesSelected12 = JSON.parse(localStorage.getItem('dessert-3'));
+  for(var c = 0; c < allRecipesSelected12.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected12.ingredients[c]);
+
+  }
+}
+if(localStorage.getItem('beverages-1')){
+  allRecipesSelected13 = JSON.parse(localStorage.getItem('beverages-1'));
+  for(var c = 0; c < allRecipesSelected13.ingredients.length; c++){
+    staticIngredientsArrayCreator(allRecipesSelected13.ingredients[c]);
+
+  }
+}
 if(localStorage.getItem('beverages-2')){
   allRecipesSelected14 = JSON.parse(localStorage.getItem('beverages-2'));
   for(var c = 0; c < allRecipesSelected14.ingredients.length; c++){
     staticIngredientsArrayCreator(allRecipesSelected14.ingredients[c]);
-    }
-  };
+  }
+}
 if(localStorage.getItem('beverages-3')){
   allRecipesSelected15 = JSON.parse(localStorage.getItem('beverages-3'));
   for(var c = 0; c < allRecipesSelected15.ingredients.length; c++){
     staticIngredientsArrayCreator(allRecipesSelected15.ingredients[c]);
   }
-};
+}
 // if(localStorage.getItem('beverages-3')){
-  //  allRecipesSelected16 = JSON.parse(localStorage.getItem('beverages-2'));
-  // for(var c = 0; c < allRecipesSelected16.ingredients.length; c++){
-    //     staticIngredientsArrayCreator(allRecipesSelected16.ingredients[c]);
-    //   }
-    // };
-    // if(localStorage.getItem('beverages-3')){
-      //     allRecipesSelected17 = JSON.parse(localStorage.getItem('beverages-2'));
-      //     for(var c = 0; c < allRecipesSelected17.ingredients.length; c++){
-        //         staticIngredientsArrayCreator(allRecipesSelected17.ingredients[c]);
-        //       }
-        //   };
-        // if(localStorage.getItem('beverages-3')){
+//  allRecipesSelected16 = JSON.parse(localStorage.getItem('beverages-2'));
+// for(var c = 0; c < allRecipesSelected16.ingredients.length; c++){
+//     staticIngredientsArrayCreator(allRecipesSelected16.ingredients[c]);
+//   }
+// };
+// if(localStorage.getItem('beverages-3')){
+//     allRecipesSelected17 = JSON.parse(localStorage.getItem('beverages-2'));
+//     for(var c = 0; c < allRecipesSelected17.ingredients.length; c++){
+//         staticIngredientsArrayCreator(allRecipesSelected17.ingredients[c]);
+//       }
+//   };
+// if(localStorage.getItem('beverages-3')){
 //  allRecipesSelected18 = JSON.parse(localStorage.getItem('beverages-2'));
 // for(var c = 0; c < allRecipesSelected18.ingredients.length; c++){
 //     staticIngredientsArrayCreator(allRecipesSelected18.ingredients[c]);
 //   }
 // };
 
+//Event Listener for "Start Over" Button
+var clearListClickHandler = function (event){
+  event.preventDefault();
+  localStorage.clear();
+  location.href = 'index.html';
+};
+
+var clearCart = document.getElementById('clear-cart');
+clearCart.addEventListener('click', clearListClickHandler);
+
 //==============================================================================================================================================================================
 //  Render Shopping List Table
 //==============================================================================================================================================================================
 
 // var renderAsATableHeader = function(){
-  //   var theadEl = document.createElement('thead');
-  //   for(var h = 0; h < tableHeaders.length; h++){
-    //     var thEl = document.createElement('th');
-    //     thEl.textContent = tableHeaders[h].toUpperCase;
-    //     theadEl.appendChild(thEl);
+//   var theadEl = document.createElement('thead');
+//   for(var h = 0; h < tableHeaders.length; h++){
+//     var thEl = document.createElement('th');
+//     thEl.textContent = tableHeaders[h].toUpperCase;
+//     theadEl.appendChild(thEl);
 //     if([r].location === tableHeaders[h]){
-  //       var renderAsATableBody = function(){
-    //         for(var i = 0; i < ingredients.length; i++) {
+//       var renderAsATableBody = function(){
+//         for(var i = 0; i < ingredients.length; i++) {
 //           var tbEl = document.getElementById('tbody');
 //           var trEl = document.createElement('tr');
 //           tdEl = document.createElement('td');
